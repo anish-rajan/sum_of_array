1598e3906aba6fb6d5251b63462beef567de05eb
Gaurav Goel <42717408+Gaurav1309Goel@users.noreply.github.com>
2018-10-02 12:34:48 +0530

    Update q3.py

--- python/a3/q3.py
+++ python/a3/q3.py
@@ -1,27 +1,27 @@
 a=input()
-b=input()
+c=input()
 y=1
 #using for loop
-for i in range(b):
+for i in range(c):
     y=y*a
 
 #using while loop
 
 i=0
 y1=1
-while(i<b):
+while(i<c):
     y1=y1*a
     i+=1
 
 #using funtion
 
-def power(a,b,i=0):
-    if i<b:
+def power(a,c,i=0):
+    if i<c:
         i+=1
-        return a*power(a,b,i)
+        return a*power(a,c,i)
     else:
         return 1
 
 print y
 print y1
-print power(a,b)
+print power(a,c)
