0160e4eeb00f72e890260a6ccd8b4f7f373b8bd2
Gaurav Goel <42717408+Gaurav1309Goel@users.noreply.github.com>
2018-10-02 12:35:57 +0530

    Update q4.py

--- python/a3/q4.py
+++ python/a3/q4.py
@@ -1,7 +1,7 @@
-def factorial(n):
-    if n==0:
+def factorial(m):
+    if m==0:
         return 1
     else:
-        return n*factorial(n-1)
-n=input()
-print factorial(n)
+        return m*factorial(m-1)
+m=input()
+print factorial(m)
